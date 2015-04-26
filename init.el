71c6c0ed5d08cc7c476dd89785043cebed8fedb5
Jøhannes Lippmann <code@schauderbasis.de>
2015-04-26 17:27:25 +0200

    Backup magic: Stop creating those something~ files all over the place. They will be accumulated in the ~/emacs.d/autosaves directory.

--- init.el
+++ init.el
@@ -65,3 +65,15 @@
 (global-set-key (kbd "M-<up>") 'windmove-up)
 (global-set-key (kbd "M-<left>") 'windmove-left)
 (global-set-key (kbd "M-<right>") 'windmove-right)
+
+
+
+
+
+
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
