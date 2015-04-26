24137319d46cb7d703e655a248fe23deb2e52d17
Jøhannes Lippmann <code@schauderbasis.de>
2015-04-26 17:25:34 +0200

    Added some key bindings that make emacs windows behave (a bit) like those of a sane tiling window manager. The key to press when switching windows is M.

--- init.el
+++ init.el
@@ -27,3 +27,41 @@
 (define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
 (define-key isearch-mode-map (kbd "C-S-f") 'isearch-repeat-backward)
 (setq isearch-lazy-highlight nil)
+
+
+
+
+
+
+;; emacsd-tile.el -- tiling windows for emacs
+;; Found here: https://gist.github.com/mariusae/287633
+;; Short blogpost here: http://monkey.org/~marius/emacs-as-a-tiling-window-manager.html
+
+(defun swap-with (dir)
+  (interactive)
+  (let ((other-window (windmove-find-other-window dir)))
+    (when other-window
+      (let* ((this-window  (selected-window))
+             (this-buffer  (window-buffer this-window))
+             (other-buffer (window-buffer other-window))
+             (this-start   (window-start this-window))
+             (other-start  (window-start other-window)))
+        (set-window-buffer this-window  other-buffer)
+        (set-window-buffer other-window this-buffer)
+        (set-window-start  this-window  other-start)
+        (set-window-start  other-window this-start)))))
+ 
+(global-set-key (kbd "M-S-<down>") (lambda () (interactive) (swap-with 'down)))
+(global-set-key (kbd "M-S-<up>") (lambda () (interactive) (swap-with 'up)))
+(global-set-key (kbd "M-S-<left>") (lambda () (interactive) (swap-with 'left)))
+(global-set-key (kbd "M-S-<right>") (lambda () (interactive) (swap-with 'right)))
+
+(global-set-key (kbd "M-C-S-<down>") (lambda () (interactive) (enlarge-window 1)))
+(global-set-key (kbd "M-C-S-<up>") (lambda () (interactive) (enlarge-window -1)))
+(global-set-key (kbd "M-C-S-<left>") (lambda () (interactive) (enlarge-window -1 t)))
+(global-set-key (kbd "M-C-S-<right>") (lambda () (interactive) (enlarge-window 1 t)))
+
+(global-set-key (kbd "M-<down>") 'windmove-down)
+(global-set-key (kbd "M-<up>") 'windmove-up)
+(global-set-key (kbd "M-<left>") 'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
