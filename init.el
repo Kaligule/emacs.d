577787a93995ea39e9f4ee332cc77e7745fe4afc
Jøhannes Lippmann <code@schauderbasis.de>
2015-05-01 15:36:30 +0200

    Better move-line now. It preserves the courser position.

--- init.el
+++ init.el
@@ -130,21 +130,31 @@
 ;; Move lines up and down
 ;; Found here: http://www.emacswiki.org/emacs/MoveLine
 
-(defun move-line-up ()
-  (interactive)
-  (transpose-lines 1)
-  (forward-line -2))
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
 
-(defun move-line-down ()
-  (interactive)
-  (forward-line 1)
-  (transpose-lines 1)
-  (forward-line -1))
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
 
-(global-set-key (kbd "M-g") 'move-line-up)
-(global-set-key (kbd "M-h") 'move-line-down)
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
 
-
+(global-set-key (kbd "<C-M-up>") 'move-line-up)
+(global-set-key (kbd "<C-M-down>") 'move-line-down)
 
 ;; Editing things
 
